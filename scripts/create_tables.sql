b53606ace73e0a9acb4950455d8693fd1c8bbd72
pgpetrov2001 <pgpetrov2001@gmail.com>
2024-07-01 11:58:03 +0300

    removed additional field in tables created in the beginning

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -1,11 +1,9 @@
 create table pedestrian_network (
 	id serial primary key,
-	name varchar(255),
 	geom geometry(MULTILINESTRING, 7801)
 );
 
 create table places (
 	id serial primary key,
-	name varchar(255),
 	geom geometry(POINT, 7801)
 );
